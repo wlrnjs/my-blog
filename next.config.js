1ddc3c0e46fab404bb29e4ec5b72703bc2413182
wlrnjs <wlrnjs5824@gmail.com>
2025-12-05 01:58:26 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/blog" : "", // GitHub 저장소 이름
+  assetPrefix: process.env.NODE_ENV === "production" ? "/blog/" : "", // GitHub 저장소 이름
 };
 
 module.exports = nextConfig;
