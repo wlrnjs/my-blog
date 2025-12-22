a6760ca620af9d49c4d613be2ced4638d70bc4f2
wlrnjs <wlrnjs5824@gmail.com>
2025-12-23 02:06:00 +0900

    feat: 상세페이지 라우팅 추가

---
+++ src/app/(route)/projects/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import { Article } from "@/shared/ui";
+
+const page = () => {
+  return (
+    <Article title="상세페이지" intro="프로젝트 상세페이지">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda voluptate rem
+        soluta minima enim, modi libero eius. Natus commodi modi aut molestiae ad. Dicta neque
+        ducimus obcaecati in veritatis?
+      </p>
+    </Article>
+  );
+};
+
+export default page;
