ddb607c01b915018c31215489b9b92c618df7689
wlrnjs <wlrnjs5824@gmail.com>
2025-12-23 05:08:24 +0900

    feat: Tags 상세페이지 임시 커밋

---
+++ src/app/(route)/tags/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { Article } from "@/shared/ui";
+
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
+
+  return (
+    <Article title={`#${slug}`}>
+      <h2>상세페이지 공사중..</h2>
+    </Article>
+  );
+};
+
+export default Page;
