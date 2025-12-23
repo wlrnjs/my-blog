5b3d3e36e4f50c8d0fc60a9ecde09f9cec91f09f
wlrnjs <wlrnjs5824@gmail.com>
2025-12-24 04:20:33 +0900

    rename: header 네이밍 변경

--- src/widgets/header/ui/Header_map.tsx
+++ src/widgets/header/ui/Header_map.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/shared/lib";
-import { NAV_ITEMS } from "../model/NAVIGATION_map";
+import { NAV_ITEMS } from "../model/NAVIGATION";
 import { ModeToggle } from "@/features/mode-toggle";
 
 const HeaderNav = () => {
