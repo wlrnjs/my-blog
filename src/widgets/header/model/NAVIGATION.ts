b4ef78e55cabd79b4c06c5beeba88767d7ada698
wlrnjs <wlrnjs5824@gmail.com>
2025-12-24 02:54:05 +0900

    refactor: header 상수 네이밍 변경

---
+++ src/widgets/header/model/NAVIGATION.ts
@@ -0,0 +1,6 @@
+export const NAV_ITEMS = [
+  { label: "Home", href: "/" },
+  { label: "Tags", href: "/tags" },
+  { label: "Projects", href: "/projects" },
+  { label: "About", href: "/about" },
+] as const;
