02aa5fbd576d6109265e288281b47068fd13f07b
wlrnjs <wlrnjs5824@gmail.com>
2025-12-24 05:34:36 +0900

    refactor: global.css 분리

--- src/app/(route)/posts/[slug]/page.tsx
+++ src/app/(route)/posts/[slug]/page.tsx
@@ -1,11 +1,17 @@
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
-import { Article } from "@/shared/ui";
-import { getPostBySlug } from "@/entities/post/api";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { Article } from "@/shared/ui";
+import { getPostBySlug } from "@/entities/post/api";
+
 import "highlight.js/styles/github-dark.css";
+import "@/entities/post/styles/markdown.css";
+import "@/entities/post/styles/highlight.css";
+import "@/entities/post/styles/markdown-anchor.css";
 
 interface PostProps {
   params: Promise<{ slug: string }>;
@@ -23,7 +29,18 @@ export default async function PostPage({ params }: PostProps) {
         <article className="prose max-w-none dark:prose-invert">
           <ReactMarkdown
             remarkPlugins={[remarkGfm, remarkBreaks]}
-            rehypePlugins={[rehypeHighlight]}
+            rehypePlugins={[
+              rehypeHighlight,
+              rehypeSlug,
+              [
+                rehypeAutolinkHeadings,
+                {
+                  behavior: "append",
+                  properties: { className: ["heading-anchor"] },
+                  content: [{ type: "text", value: "#" }],
+                },
+              ],
+            ]}
           >
             {post.description}
           </ReactMarkdown>
