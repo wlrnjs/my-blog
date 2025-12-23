51a1ebcf3c29fc513ef6c0179eedfbe8d317ce30
wlrnjs <wlrnjs5824@gmail.com>
2025-12-24 03:40:29 +0900

    rename: article 네이밍 변경

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,4 @@
-export { default as Article } from "./Article/Article";
+export { default as Article } from "./article/Article";
 export { default as GitHubIcon } from "./icons/GitHubIcon";
 export { default as GmailIcon } from "./icons/GmailIcon";
 export { default as InstagramIcon } from "./icons/InstagramIcon";
