9fb1ac9f11ea8651d6e97471fa5eed8b87f4aaf2
wlrnjs <wlrnjs5824@gmail.com>
2025-12-24 04:20:51 +0900

    rename: header 네이밍 변경

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,2 +1,2 @@
-export { default as Header } from "./header/ui/Header_map";
+export { default as Header } from "./header/ui/Header";
 export { default as ExternalLinks } from "./external-links/ui/ExternalLinks";
