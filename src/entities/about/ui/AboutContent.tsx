cef54bef65b4d0aea7a4dcce9fd68d55e6b4a6ed
wlrnjs <wlrnjs5824@gmail.com>
2025-12-25 23:48:10 +0900

    design: About 페이지 텍스트 색상 수정

--- src/entities/about/ui/AboutContent.tsx
+++ src/entities/about/ui/AboutContent.tsx
@@ -7,11 +7,11 @@ const AboutContent = () => {
   return (
     <div className="space-y-5">
       <AboutSection title="소개">
-        <p>{ABOUT_REASON}</p>
+        <p className="dark:text-white">{ABOUT_REASON}</p>
       </AboutSection>
 
       <AboutSection title="관심 분야">
-        <ul className="ml-2 list-inside list-disc space-y-1 text-sm">
+        <ul className="ml-2 list-inside list-disc space-y-1 text-sm dark:text-white">
           {ABOUT_INTERESTS.map((item) => (
             <li key={item}>{item}</li>
           ))}
@@ -19,7 +19,7 @@ const AboutContent = () => {
       </AboutSection>
 
       <AboutSection title="개발 스타일 / 가치관">
-        <p>{ABOUT_STYLE}</p>
+        <p className="dark:text-white">{ABOUT_STYLE}</p>
       </AboutSection>
 
       <EquipmentSection />
