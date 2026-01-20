dce3bdde6a13f75ca00bdf2e124e78638d592c21
wlrnjs <wlrnjs5824@gmail.com>
2026-01-21 05:35:39 +0900

    feat: 메인페이지 우측 사이드바 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem enableColorScheme>
-          <div className="mx-auto max-w-2xl px-4 py-10">
+          <div className="mx-auto max-w-4xl px-4 py-10">
             <Header />
             <main>{children}</main>
           </div>
