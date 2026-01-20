98b35deeb0d26c5ec32d191ca024e3734ee3a53f
wlrnjs <wlrnjs5824@gmail.com>
2026-01-21 06:01:56 +0900

    style: About 페이지 텍스트 색상 및 크기 조절

--- src/entities/about/ui/AboutContent.tsx
+++ src/entities/about/ui/AboutContent.tsx
@@ -5,9 +5,13 @@ import { ABOUT_INTERESTS, ABOUT_REASON, ABOUT_STYLE } from "../model/ABOUT";
 
 const AboutContent = () => {
   return (
-    <div className="space-y-5">
+    <div className="space-y-6">
       <AboutSection title="소개" muted={false}>
         <p className="whitespace-pre-line">{ABOUT_REASON}</p>
+      </AboutSection>
+
+      <AboutSection title="개발 스타일 / 가치관" muted={false}>
+        <p className="whitespace-pre-line">{ABOUT_STYLE}</p>
       </AboutSection>
 
       <AboutSection title="관심 분야" muted={false}>
@@ -16,10 +20,6 @@ const AboutContent = () => {
             <li key={item}>{item}</li>
           ))}
         </ul>
-      </AboutSection>
-
-      <AboutSection title="개발 스타일 / 가치관" muted={false}>
-        <p className="whitespace-pre-line">{ABOUT_STYLE}</p>
       </AboutSection>
 
       <EquipmentSection />
