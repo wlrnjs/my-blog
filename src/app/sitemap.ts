d82b475cd67cf56d3cd73f722f6e5c800f03f357
wlrnjs <wlrnjs5824@gmail.com>
2026-01-23 03:37:43 +0900

    fix: sitemap projects 페이지 주석

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -24,11 +24,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.6,
     },
-    {
-      url: `${baseUrl}/projects`,
-      lastModified: now,
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
+    // {
+    //   url: `${baseUrl}/projects`,
+    //   lastModified: now,
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
   ];
 }
